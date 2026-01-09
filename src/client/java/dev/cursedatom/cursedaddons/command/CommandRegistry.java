cee1379e075b5305845ca222c49a0304415a76ae
CursedAtom <cursedatom@gmail.com>
2026-01-09 18:07:50 -0500

    Add a fakechat command with tellraw syntax

--- src/client/java/dev/cursedatom/cursedaddons/command/CommandRegistry.java
+++ src/client/java/dev/cursedatom/cursedaddons/command/CommandRegistry.java
@@ -1,6 +1,11 @@
 package dev.cursedatom.cursedaddons.command;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+import com.mojang.serialization.JsonOps;
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import dev.cursedatom.cursedaddons.utils.LoggerUtils;
 import dev.cursedatom.cursedaddons.utils.MessageUtils;
@@ -8,7 +13,10 @@ import dev.cursedatom.cursedaddons.utils.TextUtils;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentSerialization;
 
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class CommandRegistry {
@@ -28,6 +36,11 @@ public class CommandRegistry {
                         return Command.SINGLE_SUCCESS;
                     })
                 )
+                .then(literal("fakechat")
+                    .then(argument("json", StringArgumentType.greedyString())
+                        .executes(CommandRegistry::executeFakeChat)
+                    )
+                )
             );
         });
     }
@@ -41,5 +54,22 @@ public class CommandRegistry {
                 MessageUtils.sendToNonPublicChat(TextUtils.trans("text.cursedaddons.error.gui_open_failed", e.getMessage()));
             }
          });
+    }
+
+    private static int executeFakeChat(CommandContext<FabricClientCommandSource> context) {
+        String json = StringArgumentType.getString(context, "json");
+        try {
+            Gson gson = new Gson();
+            JsonElement jsonElement = gson.fromJson(json, JsonElement.class);
+            Component component = ComponentSerialization.CODEC.parse(JsonOps.INSTANCE, jsonElement).getOrThrow();
+            MessageUtils.sendToNonPublicChat(component);
+        } catch (JsonSyntaxException e) {
+            MessageUtils.sendToNonPublicChat(TextUtils.trans("text.cursedaddons.error.invalid_json", e.getMessage()));
+            LoggerUtils.error("Invalid JSON in fakechat command: " + json + " - " + e.getMessage());
+        } catch (Exception e) {
+            MessageUtils.sendToNonPublicChat(TextUtils.trans("text.cursedaddons.error.fakechat_failed", e.getMessage()));
+            LoggerUtils.error("Failed to execute fakechat command: " + e.getMessage());
+        }
+        return Command.SINGLE_SUCCESS;
     }
 }
